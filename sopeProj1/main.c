2957800ae084955b8dde083a4b8e02760b15991a
Eduardo Almeida <megaedux@me.com>
2014-03-28 13:41:09 +0000

    Initial Commit

---
+++ sopeProj1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  sopeProj1
+//
+//  Created by Eduardo Almeida on 28/03/14.
+//  Copyright (c) 2014 Bitten Apps. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
